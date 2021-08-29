bce63f7783835974d08f24b922a1f3f9ef1be21e
HellBender2011 <palashdubey11@gmail.com>
2021-08-29 22:21:17 +0530

    1

---
+++ 1TheGlassHouse.java
@@ -0,0 +1,14 @@
+
+public class 1TheGlassHouse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		exitDoor(99);
+	}
+	public static int exitDoor(int N){
+		int firstDigit = N%10;
+		int secondDigit = N/10;
+		return(firstDigit + secondDigit);
+	}
+
+}
